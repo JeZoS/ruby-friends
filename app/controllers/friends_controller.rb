ceb602be11760ca94cf0078fc3bfc07af2fc569f
JeZoS <prabhat262111@gmail.com>
2021-05-27 20:13:04 +0530

    Added authentication

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,13 +1,20 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /friends or /friends.json
   def index
-    @friends = Friend.all
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    else
+      @friends = current_user.friends
+    end
   end
 
   # GET /friends/1 or /friends/1.json
   def show
+    if @friend.user != current_user
+      redirect_to root_path
+    end
   end
 
   # GET /friends/new
@@ -17,12 +24,15 @@ class FriendsController < ApplicationController
 
   # GET /friends/1/edit
   def edit
+    if @friend.user != current_user
+      redirect_to root_path
+    end
   end
 
   # POST /friends or /friends.json
   def create
     @friend = Friend.new(friend_params)
-
+    @friend.user = current_user
     respond_to do |format|
       if @friend.save
         format.html { redirect_to @friend, notice: "Friend was successfully created." }
@@ -36,19 +46,26 @@ class FriendsController < ApplicationController
 
   # PATCH/PUT /friends/1 or /friends/1.json
   def update
-    respond_to do |format|
-      if @friend.update(friend_params)
-        format.html { redirect_to @friend, notice: "Friend was successfully updated." }
-        format.json { render :show, status: :ok, location: @friend }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @friend.errors, status: :unprocessable_entity }
+    if @friend.user != current_user
+      redirect_to root_path
+    else
+      respond_to do |format|
+        if @friend.update(friend_params)
+          format.html { redirect_to @friend, notice: "Friend was successfully updated." }
+          format.json { render :show, status: :ok, location: @friend }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @friend.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   # DELETE /friends/1 or /friends/1.json
   def destroy
+    if @friend.user != current_user
+      redirect_to root_path
+    end
     @friend.destroy
     respond_to do |format|
       format.html { redirect_to friends_url, notice: "Friend was successfully destroyed." }
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,6 @@
 class Friend < ApplicationRecord
+   validates :first_name , presence: true
+   validates :last_name , presence: true
+   validates :phone , presence: true
+   belongs_to :user
 end
